d2f508af7910f00ad3d97cfe38b81d978f6bccfc
Raif Harik <harik.raif@gmail.com>
2026-03-11 10:45:15 -0500

    removing some cruft

--- apps/api/src/domain/types.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Shared domain types used by multiple entities.
- */
-
-import type { ResourceTypeEnum } from "@photo-app/contracts";
-
-/** Resource that can be shared via ShareLink or commented on. */
-export type ResourceKind = ResourceTypeEnum;
